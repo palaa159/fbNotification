694cdf766b0c64e9ba952142bd19eef61acf84d9
Apon Palanwuech <palaa159@newschool.edu>
2016-01-22 14:18:09 +0700

    - update

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var fb = require('facebook-chat-api')
 var express = require('express')
+var bodyParser = require('body-parser')
 var s = express()
 var Observer = require('./observer')
 var observable = new Observer()
 
-// console.log(process.env.FB_USER)
+// Bitly
+var Bitly = require('bitly');
+var bitly = new Bitly('c511ade28b90c083955b68f42f5daaf53eb69ae6');
+
+// URIjs
+var URI = require('urijs');
 
 // init
 fb({
@@ -14,9 +20,12 @@ fb({
 	if (err) return console.error(err)
 		// Custom observable
 	observable.on('send', function(obj) {
-		api.sendMessage(obj.message, obj.id, function() {
-			console.log('Message: "' + 
-				obj.message + 
+		api.sendMessage(obj.message, obj.id, function(err) {
+			if (err) {
+				return console.error(err)
+			}
+			console.log('Message: "' +
+				obj.message +
 				'" was sent to ' +
 				obj.id + ' on ' +
 				new Date())
@@ -27,14 +36,47 @@ fb({
 	// 	observable.send('hello apon', 681777115)
 	// }, 2000)
 })
+s.use(bodyParser.json())
+	.use(bodyParser.urlencoded({
+		extended: false
+	}))
+	.post('/message', newMessage)
+	.listen(4555, function() {
 
-s.post('/message', newMessage)
-
-s.listen(4555, function() {
-
-})
+	})
 
 // Callbacks
+var goToSettingMessage = '\n\nขณะนี้เรากำลังทดสอบระบบการแจ้งเตือนด้วย Facebook ท่านสามารถเข้าไปปรับค่า Setting การรับข้อความได้ในเมนู "ตั้งค่า" ใน Account Fastwork.co ของท่าน'
+
 function newMessage(req, res) {
-	observable.send('คุณได้รับข้อความใหม่: ', 681777115)
+	extractAndBitly(req.body.message, function(extracted) {
+		observable.send(extracted + goToSettingMessage, req.body.id)
+		res.json({
+			success: true
+		})
+	})
+}
+
+// Extract message and bitly the link
+function extractAndBitly(message, cb) {
+	var url = URI.withinString(message, function(url) {
+		bitly.shorten(url)
+			.then(function(response) {
+				var short_url = response.data.url
+
+				message = message.replace(url, short_url)
+
+				if (cb) {
+					cb(message)
+				}
+				// Do something with data
+			}, function(error) {
+				throw error;
+			});
+		return false;
+	});
+
+	// Find url
+	// Regex url
+
 }
